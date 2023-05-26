4f7bc60e61e652277f197f74e7f1005a77f9707b
qiankong <962764322@qq.com>
2023-05-26 17:17:15 +0800

    feaut: add build config

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+// vite.config.js
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'packages/core/index.ts'),
+      name: 'VueSurplus',
+      fileName: 'vue-surplus',
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+})
